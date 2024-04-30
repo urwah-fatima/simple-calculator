02f89dfd694dd412d56478a52d35d0489e548aa6
urwah-fatima <urwah209fatima@gmail.com>
2024-04-30 15:25:48 +0500

    created this calculator using TYPESCRIPT, NODE.JS AND NQUIRER

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enetr first number", type: "number", name: "firstNumber" },
+    { message: "Enetr second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform actions",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// Applying conditional statements:
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select the valid operator");
+}
